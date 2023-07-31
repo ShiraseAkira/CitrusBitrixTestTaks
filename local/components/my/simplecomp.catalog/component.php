5f1f50d27cd297ffc85719eb3bdb1bd93fa486d9
Maxim Laptev <justsaynya@gmail.com>
2023-07-31 20:14:26 +0300

    feat: component logic implemented

--- local/components/my/simplecomp.catalog/component.php
+++ local/components/my/simplecomp.catalog/component.php
@@ -1,117 +1,105 @@
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-/** @var CBitrixComponent $this */
-/** @var array $arParams */
-/** @var array $arResult */
-/** @var string $componentPath */
-/** @var string $componentName */
-/** @var string $componentTemplate */
-/** @global CDatabase $DB */
-/** @global CUser $USER */
-/** @global CMain $APPLICATION */
 
+if(!CModule::IncludeModule("iblock"))
+{
+	ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
+	return;
+}
 
-/*************************************************************************
-	Processing of received parameters
-*************************************************************************/
-if(!isset($arParams["CACHE_TIME"]))
+//
+if(empty($arParams["CACHE_TIME"]))
 	$arParams["CACHE_TIME"] = 180;
+$arParams["IBLOCK_CATALOG_ID"] = (int)$arParams["IBLOCK_CATALOG_ID"];
+$arParams["IBLOCK_NEWS_ID"] = (int)$arParams["IBLOCK_NEWS_ID"];
 
-if(!is_array($arParams["IBLOCKS"]))
-	$arParams["IBLOCKS"] = array($arParams["IBLOCKS"]);
-
-$arIBlockFilter = array();
-foreach($arParams["IBLOCKS"] as $IBLOCK_ID)
-{
-	$IBLOCK_ID=intval($IBLOCK_ID);
-	if($IBLOCK_ID>0)
-		$arIBlockFilter[]=$IBLOCK_ID;
-}
-
-if(empty($arIBlockFilter))
-{
-	if(!CModule::IncludeModule("iblock"))
-	{
-		ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
+if ($this->startResultCache()) {
+	if ($arParams["IBLOCK_CATALOG_ID"] == 0 ||
+		$arParams["IBLOCK_NEWS_ID"] == 0 ||
+		empty($arParams['USER_CATALOG_NEWS_BINDING_PROPERTY'])
+	) {
+		$this->abortResultCache();
 		return;
 	}
-	$rsIBlocks = CIBlock::GetList(array("sort" => "asc"), array(
-		"type" => $arParams["IBLOCK_TYPE"],
-		"LID" => SITE_ID,
-		"ACTIVE" => "Y",
-	));
-	if($arIBlock = $rsIBlocks->Fetch())
-		$arIBlockFilter[] = $arIBlock["ID"];
-}
 
-unset($arParams["IBLOCK_TYPE"]);
-$arParams["PARENT_SECTION"] = intval($arParams["PARENT_SECTION"]);
-$arParams["IBLOCKS"] = $arIBlockFilter;
+	//Getting active news
+	$arNewsID = [];
 
-if(!empty($arIBlockFilter) && $this->StartResultCache(false, ($arParams["CACHE_GROUPS"]==="N"? false: $USER->GetGroups())))
-{
-	if(!CModule::IncludeModule("iblock"))
-	{
-		$this->AbortResultCache();
-		ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
-		return;
+	$arSelect = [
+		'ID',
+		'NAME',
+		'ACTIVE_FROM',
+	];
+	$arFilter = [
+		'IBLOCK_ID' => $arParams["IBLOCK_NEWS_ID"],
+		'ACTIVE' => 'Y',
+	];
+	$resNews = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
+
+	while ($newsElement = $resNews->Fetch()) {
+		$arNewsID[] = $newsElement['ID'];
+		$arResult['NEWS'][$newsElement['ID']] = $newsElement;
 	}
-	//SELECT
-	$arSelect = array(
-		"ID",
-		"IBLOCK_ID",
-		"CODE",
-		"IBLOCK_SECTION_ID",
-		"NAME",
-		"PREVIEW_PICTURE",
-		"DETAIL_PICTURE",
-		"DETAIL_PAGE_URL",
-	);
-	//WHERE
-	$arFilter = array(
-		"IBLOCK_ID" => $arParams["IBLOCKS"],
-		"ACTIVE_DATE" => "Y",
-		"ACTIVE"=>"Y",
-		"CHECK_PERMISSIONS"=>"Y",
-	);
-	if($arParams["PARENT_SECTION"]>0)
-	{
-		$arFilter["SECTION_ID"] = $arParams["PARENT_SECTION"];
-		$arFilter["INCLUDE_SUBSECTIONS"] = "Y";
-	}
-	//ORDER BY
-	$arSort = array(
-		"RAND"=>"ASC",
-	);
-	//EXECUTE
-	$rsIBlockElement = CIBlockElement::GetList($arSort, $arFilter, false, false, $arSelect);
-	$rsIBlockElement->SetUrlTemplates($arParams["DETAIL_URL"]);
-	if($arResult = $rsIBlockElement->GetNext())
-	{
-		$arResult["PICTURE"] = CFile::GetFileArray($arResult["PREVIEW_PICTURE"]);
-		if(!is_array($arResult["PICTURE"]))
-			$arResult["PICTURE"] = CFile::GetFileArray($arResult["DETAIL_PICTURE"]);
 
-		$ipropValues = new \Bitrix\Iblock\InheritedProperty\ElementValues($arResult["IBLOCK_ID"], $arResult["ID"]);
-		$arResult["IPROPERTY_VALUES"] = $ipropValues->getValues();
+	//Getting active sections binded to news
+	$arSections = [];
+	$arSectionsID = [];
 
-		if ($arResult["PICTURE"])
-		{
-			$arResult["PICTURE"]["ALT"] = $arResult["IPROPERTY_VALUES"]["ELEMENT_PREVIEW_PICTURE_FILE_ALT"];
-			if ($arResult["PICTURE"]["ALT"] == "")
-				$arResult["PICTURE"]["ALT"] = $arResult["NAME"];
-			$arResult["PICTURE"]["TITLE"] = $arResult["IPROPERTY_VALUES"]["ELEMENT_PREVIEW_PICTURE_FILE_TITLE"];
-			if ($arResult["PICTURE"]["TITLE"] == "")
-				$arResult["PICTURE"]["TITLE"] = $arResult["NAME"];
+	$arSelect = [
+		'ID',
+		'NAME',
+		$arParams['USER_CATALOG_NEWS_BINDING_PROPERTY'],
+	];
+	$arFilter = [
+		'IBLOCK_ID' => $arParams["IBLOCK_CATALOG_ID"],
+		'ACTIVE' => 'Y',
+		$arParams['USER_CATALOG_NEWS_BINDING_PROPERTY'] => $arNewsID,
+	];
+	$resSections = CIBlockSection::GetList([], $arFilter, false, $arSelect, false);
+
+	while ($sectionElement = $resSections->Fetch()) {
+		$arSectionsID[] = $sectionElement['ID'];
+		$arSections[$sectionElement['ID']] = $sectionElement;
+
+		foreach ($sectionElement[$arParams['USER_CATALOG_NEWS_BINDING_PROPERTY']] as $newsID) {
+			$arResult['NEWS'][$newsID]['SECTIONS'][] = $sectionElement['NAME'];
 		}
+	}
 
-		$this->SetResultCacheKeys(array(
-		));
-		$this->IncludeComponentTemplate();
+	//Getting active products
+	$arProducts = [];
+	$arProductsID = [];
+
+	$arSelect = [
+		'ID',
+		'IBLOCK_SECTION_ID',
+		'NAME',
+		'PROPERTY_MATERIAL',
+		'PROPERTY_ARTNUMBER',
+		'PROPERTY_PRICE',
+	];
+	$arFilter = [
+		'IBLOCK_ID' => $arParams["IBLOCK_CATALOG_ID"],
+		'ACTIVE' => 'Y',
+		'SECTION_ID' => $arSectionsID,
+	];
+	$resProducts = CIBlockElement::GetList([], $arFilter, false, false, $arSelect);
+
+	$arResult['PRODUCT_COUNT'] = 0;
+	while ($productsElement = $resProducts->Fetch()) {
+		$arResult['PRODUCT_COUNT']++;
+
+		$productSectionID = $productsElement['IBLOCK_SECTION_ID'];
+		$productSection = $arSections[$productSectionID];
+		$section2NewsBind = $productSection[$arParams['USER_CATALOG_NEWS_BINDING_PROPERTY']];
+		foreach($section2NewsBind as $newsID) {
+			$arResult['NEWS'][$newsID]['PRODUCT'][] = $productsElement;
+		}
 	}
-	else
-	{
-		$this->AbortResultCache();
-	}
+
+	$this->SetResultCacheKeys(['PRODUCT_COUNT']);
+
+	$this->IncludeComponentTemplate();
 }
-?>
+
+$APPLICATION->SetTitle(GetMessage('SIMPLECOMP_ELEMENTS_COUNT', ['PRODUCT_COUNT'=> $arResult['PRODUCT_COUNT']]));
--- local/components/my/simplecomp.catalog/lang/ru/component.php
+++ local/components/my/simplecomp.catalog/lang/ru/component.php
@@ -1,3 +1,4 @@
 <?
 $MESS ['IBLOCK_MODULE_NOT_INSTALLED'] = "Модуль Информационных блоков не установлен";
+$MESS ['SIMPLECOMP_ELEMENTS_COUNT'] = "В каталоге товаров представлено товаров: PRODUCT_COUNT";
 ?>
