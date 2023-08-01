e33c556cef6568ce5fd22eb744acbc1e3c3847fe
Maxim Laptev <justsaynya@gmail.com>
2023-08-01 11:18:20 +0300

    fix: synced files with remote

--- simplecomp/index.php
+++ simplecomp/index.php
@@ -2,15 +2,20 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("simplecomp");
 ?><?$APPLICATION->IncludeComponent(
-	"my:simplecomp.catalog",
-	"",
-	Array(
+	"my:simplecomp.catalog", 
+	".default", 
+	array(
 		"CACHE_GROUPS" => "Y",
 		"CACHE_TIME" => "180",
 		"CACHE_TYPE" => "A",
 		"DETAIL_URL" => "",
-		"IBLOCKS" => array(),
+		"IBLOCKS" => "",
 		"IBLOCK_TYPE" => "news",
-		"PARENT_SECTION" => ""
-	)
+		"PARENT_SECTION" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_CATALOG_ID" => "2",
+		"IBLOCK_NEWS_ID" => "1",
+		"USER_CATALOG_NEWS_BINDING_PROPERTY" => "UF_NEWS_LINK"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
